193ae81f99901cb64cd13a0946ff4fcc6e0f06d5
qingge <1823295186@qq.com>
2019-02-21 17:37:29 +0800

    dfgdfghdfh

--- 苏宁/苏宁易购/js/core.js
+++ 苏宁/苏宁易购/js/core.js
@@ -30,21 +30,21 @@ $(function(){
 		$('.ng-sn-site-na').slideUp(200);
 	});
 });
-$(function(){	
-	/*轮播图右侧评论*/
-	var $pinglun =  $('.toutiao').find('ul').eq(0);
-	function move(){
-		var $pinglunJl = $pinglun.css('marginTop');
-		if($pinglunJl === '-288px'){
-			$pinglun.css({marginTop:'0px'})
-		}
-		$pinglun.animate({marginTop:'+=-144px'})
-	}
-	timer = setInterval(move,2000)
-	$pinglun.hover(function(){
-		clearInterval(timer)
-	},function(){
-		setInterval(move,2000)
-	})
+// $(function(){	
+// 	/*轮播图右侧评论*/
+// 	var $pinglun =  $('.toutiao').find('ul').eq(0);
+// 	function move(){
+// 		var $pinglunJl = $pinglun.css('marginTop');
+// 		if($pinglunJl === '-288px'){
+// 			$pinglun.css({marginTop:'0px'})
+// 		}
+// 		$pinglun.animate({marginTop:'+=-144px'})
+// 	}
+// 	timer = setInterval(move,2000)
+// 	$pinglun.hover(function(){
+// 		clearInterval(timer)
+// 	},function(){
+// 		setInterval(move,2000)
+// 	})
 
-})
+// })
