804b7e9e66e3b22b0d77038c301b07af94ba1ec3
qingge <1823295186@qq.com>
2019-03-06 20:59:05 +0800

    aa6

--- 跨淘网/js/003.search.js
+++ 跨淘网/js/003.search.js
@@ -5,19 +5,49 @@ function Search($elem,options){
 	//1罗列属性
 	this.$elem = $elem;
 	this.options = options;
+	this.$searchBtn = $elem.find('.search-btn');//拿到第二个input框
+	this.$searchInput = $elem.find('.search-input');//拿到第一个input框
+	this.$searchForm = $elem.find('.search-form');//拿到form
 	//2初始化
 	this.init();
+	if(this.options.autocompelte){
+		this.autocompelte();
+	}
 }
 //原型对象，上做初始化
 Search.prototype = {
 	constructor:Search,
 	init:function(){	
+		//1.绑定事件
+		this.$searchBtn.on('click',$.proxy(this.submit,this));
 	},
-}
+	submit:function(){
+		if(this.getInputVal() == ''){
+			return	false//如果是空格就直接返回
+		}
+		this.$searchForm.trigger('submit')
+	},
+	getInputVal:function(){
+		return  $.trim(this.$searchInput.val());//做一个剔除空格的方法
+	},
+	autocompelte:function(){
+		//1.监听input的事件
+		this.$searchInput.on('input',$.proxy(this.getData,this));
 
+	},
+	//监听到input事件就调用下面的方法
+	getData:function(){
+		// console.log('will get data...')
+		$.ajax({
+			url:this.options.url + this.getInputVal()
+		})
+	}
+}
+//callback 回调是callback
 //静态的配置项   默认方法
 Search.DEFAULTS = {
-	js:true,
+	autocompelte:true,
+	url:"https://suggest.taobao.com/sug?code=utf-8&q="
 }
 
 //注册插件
