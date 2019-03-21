1cd327cde7b91183cc0f3c9356da29a4627a3e32
qingge <1823295186@qq.com>
2019-03-21 14:58:19 +0800

    dd3

--- nodejs/004-globa/001.process.js
+++
@@ -1,6 +0,0 @@
-// console.log(process)
-// console.log(global.process)
-// console.log(global.process == process)
-// console.log(process.argv)
-// console.log(process.env)
-console.log(process.pid)
---
+++ nodejs/04-globa/004.timer.js
@@ -0,0 +1,25 @@
+// console.log(1);
+
+// setTimeout(()=>{
+// 	console.log(2)
+// },0)
+// console.log(3)
+
+// console.log(1);
+
+// let timer = setTimeout(()=>{
+// 	console.log(2)
+// },0)
+
+// clearTimeout(timer)
+// console.log(3)
+
+
+console.log(1);
+let timer = setInterval(()=>{
+	console.log(2)
+},200)
+clearInterval(timer)
+console.log(3)
+
+
