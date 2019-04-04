c53da6677807ac9f49f155878fc3af96d04f1195
qingge <1823295186@qq.com>
2019-04-04 15:00:27 +0800

    e1

--- bootstrapxiangmu/routles/admin.js
+++ bootstrapxiangmu/routles/admin.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const UserModel = require('../models/user.js')
 const router = express.Router()
 
 
@@ -19,10 +19,65 @@ router.get("",(req,res)=>{
 	})
 })
 
+
+//显示用户列表
 router.get("/users",(req,res)=>{
-	res.render('admin/user_list',{
-		userInfo:req.userInfo
+	/*
+	分页
+	约定：每页显示2条 limit(2)
+
+	第一页 跳过 0 条 skip(0)
+	第二页 跳过 2 条 skip(2)
+	第三页 跳过 4 条 skip(4)
+
+	第 page 页 跳过 (page -1) * limit 条 skip((page -1) * limit)
+	
+	*/
+
+	let { page } = req.query;
+	const limit = 2;
+	page = parseInt(page)
+
+	if(isNaN(page)){
+		page = 1;//可以拿到数字
+	}
+	if(page ==0){
+		page = 1;
+	}
+
+	UserModel.countDocuments({})
+	.then(count=>{
+		//计算总页数
+		const pages = Math.ceil(count / limit)
+		if(page>pages){
+			page = pages
+		}
+
+		//生成页码数组
+		const list = [];
+		for(let i = 1;i<=pages;i++){
+			list.push(i)
+		}
+		//每页显示条数
+		const skip = (page -1) * limit
+
+		UserModel.find({},'-password -__v')
+		.skip(skip)
+		.limit(limit)
+		.then(users=>{
+			res.render('admin/user_list',{
+				userInfo:req.userInfo,
+				users,
+				page,
+				list
+			})
+		})
 	})
+
+
+
+
+	
 })
 
 
