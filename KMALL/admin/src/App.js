301e87a1ac6c24c7ab0b493b33510295017f3fd5
qingge <1823295186@qq.com>
2019-04-16 20:54:24 +0800

    aa10

--- KMALL/admin/src/App.js
+++ KMALL/admin/src/App.js
@@ -1,8 +1,14 @@
 
+//第二页
+
+
+
 import React,{ Component,Fragment } from 'react'
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+//引入Login组件
+//等价于引入 './pages/login/index.js'
 import Login from './pages/login'
 import Home from './pages/home'
 
@@ -13,8 +19,11 @@ class App extends Component{
 	render(){
 		return( 
 			<Router>
-				<div className="App">
+				<div className="App">					
 					<Route exact path="/" component={Home} />
+					{
+						//当匹配到路由"/login"后,渲染Login组件
+					}
 					<Route path="/login" component={Login} />
 				</div>
 			</Router>
--- KMALL/admin/src/index.js
+++ KMALL/admin/src/index.js
@@ -5,6 +5,10 @@
 * @Last Modified time: 2019-04-12 19:05:31
 */
 
+//第一页
+
+
+
 //1.整个应用的入口
 //2.webpack-config.js中的入口指定该文件
 import React from 'react'
@@ -12,6 +16,7 @@ import ReactDOM from 'react-dom'
 
 //Provider的作用是将整个应用的唯一store传递到所有的子组件
 import { Provider } from 'react-redux'
+//整个应用的唯一store
 import store from './store'
 
 import App from './App.js'
