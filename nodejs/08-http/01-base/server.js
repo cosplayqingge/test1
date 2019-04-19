a5d56e452e4e1819d31c425afe2834164ffeaf4c
qingge <1823295186@qq.com>
2019-04-19 20:38:42 +0800

    lol

--- nodejs/08-http/01-base/server.js
+++ nodejs/08-http/01-base/server.js
@@ -1,15 +1,21 @@
+/*
+* @Author: TomChen
+* @Date:   2019-03-22 19:15:42
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-03-22 19:26:39
+*/
 const http = require('http');
 
+
 const server = http.createServer((req,res)=>{
-	//req = request 可读流
-	//res = response  可写流
-	// res.setHeader('Content-Type',"text/plain;charset=utf-8");//表明返回的是一个纯文本
-	// res.write('hellow 你好');//出现乱码问题用上面的方法
-	res.setHeader('Content-Type',"text/html;charset=utf-8");//表明返回的是一个纯文本
-	res.write('<h1>hellow 你好</h1>');
-	res.end('qingge');
+	//req=request 可读流
+	//res=response 可写流
+	// res.setHeader('Content-Type',"text/plain;charset=utf-8");
+	res.setHeader('Content-Type',"text/html;charset=utf-8");
+	res.write('<h1>hello 你好</h1>');
+	res.end('kuazhu');
 });
 
 server.listen(3000,'127.0.0.1',()=>{
-	console.log('server is http.....')
+	console.log('server is running at http://127.0.0.1:3000')
 })
--- nodejs/08-http/02-router/js/index.js
+++ nodejs/08-http/02-router/js/index.js
@@ -1,1 +1,7 @@
-alert('hah')
+/*
+* @Author: TomChen
+* @Date:   2019-03-22 19:41:40
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-03-22 19:41:47
+*/
+alert('haha');
