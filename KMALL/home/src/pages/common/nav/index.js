ee6b4d0419e07afd2f0e6dab873920c6b88cdc91
qingge <1823295186@qq.com>
2019-04-24 19:54:47 +0800

    kmall2

---
+++ KMALL/home/src/pages/common/nav/index.js
@@ -0,0 +1,26 @@
+/*
+* @Author: TomChen
+* @Date:   2019-04-24 18:17:34
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-04-24 18:18:07
+*/
+require('./index.css')
+var _user = require('service/user')
+var nav = {
+	init:function(){
+		this.bindEvent();
+		return this;
+	},
+	bindEvent:function(){
+		//1.绑定退出事件
+		$('#logout').on('click',function(){
+			_user.logout(function(result){
+				console.log(result)
+			},function(msg){
+				alert(msg)
+			})
+		})
+	}
+}
+
+module.exports = nav.init();
---
+++ KMALL/home/src/pages/index/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: TomChen
+* @Date:   2019-04-23 19:24:03
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-04-24 18:19:06
+*/
+require('pages/common/nav')
+require('./index.css')
